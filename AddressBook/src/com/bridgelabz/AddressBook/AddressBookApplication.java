065196d41379548be6963b812d7dabc0bfa6c434
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-25 07:34:58 +0530

    added : initialize with welcome message

---
+++ AddressBook/src/com/bridgelabz/AddressBook/AddressBookApplication.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.AddressBook;
+
+public class AddressBookApplication {
+    public static void main(String[] args) {
+        System.out.println("!! Welcome to address book program !! ");
+    }
+}
