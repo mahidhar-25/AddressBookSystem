adf287e1ff094c06fe680257861ed217d3459f1a
mahidhar reddy <mahidhar100008@gmail.com>
2023-11-25 07:42:59 +0530

    Added : usecase 1 , created contact person class , added ability to create a new contact

--- AddressBook/src/com/bridgelabz/AddressBook/AddressBookApplication.java
+++ AddressBook/src/com/bridgelabz/AddressBook/AddressBookApplication.java
@@ -1,7 +1,42 @@
 package com.bridgelabz.AddressBook;
 
+import java.util.Scanner;
+
 public class AddressBookApplication {
+    /*
+@desc: getContactDetailsFromUser, facilitates the input of contact details from the user through the console.
+         It gathers information such as contact ID, first name, last name, address, city, pincode, phone number, and email.
+         The method prompts the user for each piece of information and ensures proper handling of input using next() and nextLine() methods.
+         After collecting the necessary details, it creates a new ContactPerson object with the provided information and returns it.
+ @param : no params
+ @return : returns an object instance of ContactPerson
+ */
+    public static ContactPerson getContactDetailsFromUser(){
+        Scanner inputContact = new Scanner(System.in);
+        String firstName , lastName , address , city , pincode , phoneNumber , email , contactId;
+        System.out.print("\nEnter contact id : ");
+        contactId = inputContact.next();
+        System.out.print("\nEnter First name : ");
+        firstName = inputContact.next();
+        System.out.print("\nEnter Last name : ");
+        lastName = inputContact.next();
+        System.out.print("\nEnter address in single line: ");
+        inputContact.nextLine();
+        address = inputContact.nextLine();
+        System.out.print("\nEnter city in single word : ");
+        city = inputContact.next();
+        System.out.print("\nEnter pincode : ");
+        pincode = inputContact.next();
+        System.out.print("\nEnter phone number : ");
+        phoneNumber = inputContact.next();
+        System.out.print("\nEnter email : ");
+        email = inputContact.next();
+        return new ContactPerson(firstName , lastName , address , city , pincode , phoneNumber , email , contactId);
+    }
     public static void main(String[] args) {
         System.out.println("!! Welcome to address book program !! ");
+        //usecase 1
+        ContactPerson newContact = getContactDetailsFromUser();
+        System.out.println("enter contact is : " + newContact);
     }
 }
---
+++ AddressBook/src/com/bridgelabz/AddressBook/ContactPerson.java
@@ -0,0 +1,231 @@
+package com.bridgelabz.AddressBook;
+/*
+@desc :
+     this class is to create a behavior of a contact person , which has details like , name , address , city , pincode , email , phonhe number
+     where we can edit , and set these details , and can print complete person details.
+     but id is immutable
+     it is a public class , we can use this class in any package
+ */
+
+public class ContactPerson {
+    public static final String EDIT_FIRST_NAME = "FirstName";
+    public static final String EDIT_LAST_NAME = "LastName";
+    public static final String EDIT_ADDRESS = "Address";
+    public static final String EDIT_CITY = "City";
+    public static final String EDIT_PHONE_NUMBER = "PhoneNumber";
+    public static final String EDIT_PIN_CODE = "Pincode";
+    public static final String EDIT_EMAIL = "Email";
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String address;
+    private String city;
+    private String pincode;
+    private String phoneNumber;
+    private String email;
+    private String id;
+
+    /*
+    @desc : default constructor , initialzes with empty values for the person
+    @param : no parameters
+    @return : don't return
+     */
+    public ContactPerson(){
+        this("" , "" , "" , "" , "" , "" , "" , "");
+    }
+
+    /*
+    @desc: This Java constructor initializes a ContactPerson object with the provided parameters,
+           setting various attributes like first name, last name, address, etc.
+           It calculates the full name by concatenating the first and last names.
+    @params:
+            firstName (String) - The first name of the contact person.
+            lastName (String) - The last name of the contact person.
+            address (String) - The address of the contact person.
+            city (String) - The city of the contact person.
+            pincode (String) - The pin code of the contact person's location.
+            phoneNumber (String) - The phone number of the contact person.
+            email (String) - The email address of the contact person.
+            id (String) - The unique identifier for the contact person.
+    @return: This constructor does not have a return type (void). It is used to initialize the attributes of the ContactPerson object.
+     */
+    public ContactPerson(String firstName, String lastName, String address, String city, String pincode, String phoneNumber, String email , String id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.pincode = pincode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.id = id;
+        this.fullName = firstName + " " + lastName;
+    }
+
+
+
+
+
+    /*
+      @desc : it gives first name of the contact person
+      @params : no parameter
+      @return : return firstName(string) of a person
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /*
+  @desc : it sets/updates the first name of the person
+  @params : no parameter
+  @return : don't return
+ */
+    public void setFirstName(String firstName) {
+        this.fullName = firstName +" "+ this.lastName;
+        this.firstName = firstName;
+    }
+    /*
+      @desc : it gives last name of the contact person
+      @params : no parameter
+      @return : return lastName(string) of a person
+     */
+    public String getLastName() {
+        return lastName;
+    }
+    /*
+  @desc : it sets/updates the last name of the person
+  @params : no parameter
+  @return : don't return
+ */
+    public void setLastName(String lastName) {
+        this.fullName = this.firstName +" "+ lastName;
+        this.lastName = lastName;
+    }
+
+    /*
+  @desc : it gives address of the contact person
+  @params : no parameter
+  @return : return address(string) of a person
+ */
+    public String getAddress() {
+        return address;
+    }
+
+    /*
+@desc : it sets/updates the  address of the person
+@params : no parameter
+@return : don't return
+*/
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /*
+  @desc : it gives city  of the contact person
+  @params : no parameter
+  @return : return city(string) of a person
+ */
+    public String getCity() {
+        return city;
+    }
+    /*
+  @desc : it sets/updates the city of the person
+  @params : no parameter
+  @return : don't return
+ */
+    public void setCity(String city) {
+        this.city = city;
+    }
+    /*
+      @desc : it gives pincode of the contact person
+      @params : no parameter
+      @return : return pincode(string) of a person
+     */
+    public String getPincode() {
+        return pincode;
+    }
+    /*
+  @desc : it sets/updates the pincode of the person
+  @params : no parameter
+  @return : don't return
+ */
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+
+    /*
+  @desc : it gives phone number of the contact person
+  @params : no parameter
+  @return : return phoneNumber(string) of a person
+ */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    /*
+  @desc : it sets/updates the phone number of the person
+  @params : no parameter
+  @return : don't return
+ */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /*
+  @desc : it gives email of the contact person
+  @params : no parameter
+  @return : return email(string) of a person
+ */
+    public String getEmail() {
+        return email;
+    }
+    /*
+      @desc : it gives full name of the contact person
+      @params : no parameter
+      @return : return fullname(string) of a person
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /*
+  @desc : it gives details of the contact person
+  @params : no parameter
+  @return : return complete person details as string
+ */
+    public String toString() {
+        return "{" +
+                "\nfirstName = " + firstName + ",\n" +
+                "lastName = " + lastName + ",\n" +
+                "fullName = " + fullName + ",\n" +
+                "address = " + address + ",\n" +
+                "city = " + city + ",\n" +
+                "pincode = " + pincode + ",\n" +
+                "phoneNumber = " + phoneNumber + ",\n" +
+                "email = " + email + ",\n" +
+                "id = " + id + "\n" +
+                "}\n";
+    }
+    /*
+  @desc : it sets/updates the email of the person
+  @params : no parameter
+  @return : don't return
+ */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    /*
+  @desc : it gives specific id  of the contact person
+  @params : no parameter
+  @return : return id(string) of a person
+ */
+
+    public String getId() {
+        return id;
+    }
+
+
+
+
+
+
+}
+
